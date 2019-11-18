1bca1d96a69b59ce7cb48457b62c2cde389737a5
Ubuntu <ubuntu@ip-172-31-94-58.ec2.internal>
2019-11-18 13:33:23 +0000

    Amended Users and Roles but need to break out the creation of tables into the separate migrations

--- studio_ghibli_store/db/migrate/20191117211306_create_users.rb
+++ studio_ghibli_store/db/migrate/20191117211306_create_users.rb
@@ -7,13 +7,16 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.integer "user_id"
       t.string "first_name", :limit => 50
       t.string "last_name", :limit => 50
+      t.string "display_name", :limit => 255
       t.string "email", :default => '', :null => false
-      t.string "password", :limit => 40
+      t.string "encrypted_password", :limit => 40
       t.bigint "mobile_number", :limit => 15
       t.bigint "phone_number", :limit => 15
+      t.integer "is_deleted", :limit => 2
       t.timestamps
     end
      create_table :roles do |t|
+      t.string "title", :limit => 50
       t.integer "user_id"
       t.timestamps
     end
---
+++ studio_ghibli_store/db/migrate/20191118131029_create_roles.rb
@@ -0,0 +1,4 @@
+class CreateRoles < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
