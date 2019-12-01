858df5840d821775ef18718724470db4eb2e4315
Craig Bell <craigdanielbell@gmail.com>
2019-12-01 01:30:05 +0000

    Added the configure_permitted_parameters to the ApplicationController to include the first name, last name and mobile to the Users db table

--- studio_ghibli_store/app/controllers/application_controller.rb
+++ studio_ghibli_store/app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    protect_from_forgery with: :exception
-    
-    def configure_permitted_parameters
-        #:display_name = :first_name + :last_name
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile_number])
-    end
+  protect_from_forgery with: :exception
 end
