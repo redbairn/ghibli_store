68c2e83a7ad30f21219f3728771d8d4d6fd8ae64
Craig Bell <craigdanielbell@gmail.com>
2019-12-01 00:50:32 +0000

    Amended the styles for the Sign-Up form and need to check out why other form values are not passing through to the db table

--- studio_ghibli_store/config/routes.rb
+++ studio_ghibli_store/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :logins
-  devise_for :user_logins
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   
     root 'static_pages#home'
