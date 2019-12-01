4f0d48fc6ee1fc759ce8e55d75b1651f84a4e5ef
Craig Bell <craigdanielbell@gmail.com>
2019-12-01 01:21:05 +0000

    First commit

--- studio_ghibli_store/config/routes.rb
+++ studio_ghibli_store/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   
-    root 'static_pages#home'
+    root to: 'static_pages#home'
     get '/home' => 'static_pages#home'
     get '/about' => 'static_pages#about'
     get '/help' => 'static_pages#help' 
