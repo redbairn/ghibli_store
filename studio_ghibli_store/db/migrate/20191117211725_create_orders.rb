0adbba2b9e457afbd7b1e871c5920829e101335a
Craig Bell <craigdanielbell@gmail.com>
2019-12-03 13:44:35 +0000

    Back to original Orderitem error - uninitialized constant

--- studio_ghibli_store/db/migrate/20191117211725_create_orders.rb
+++ studio_ghibli_store/db/migrate/20191117211725_create_orders.rb
@@ -3,6 +3,7 @@ class CreateOrders < ActiveRecord::Migration[5.0]
     create_table :orders do |t|
       t.integer "user_id"
       t.datetime :order_date
+      t.string :status
       t.timestamps
     end
     add_index :orders, :user_id
--- studio_ghibli_store/db/schema.rb
+++ studio_ghibli_store/db/schema.rb
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_163024) do
   create_table "orders", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "order_date"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
