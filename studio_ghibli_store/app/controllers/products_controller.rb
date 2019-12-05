fd02203199ad79035f4e8774e944264cad3d00eb
Craig Bell <craigdanielbell@gmail.com>
2019-12-05 22:15:58 +0000

    The categories are now setup and the products are filtered by them successfully

--- studio_ghibli_store/app/controllers/products_controller.rb
+++ studio_ghibli_store/app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :colour, :cost_price, :image_url, :catalog_categories_id, :supplier_id)
+      params.require(:product).permit(:title, :description, :colour, :cost_price, :image_url, :categories_id, :supplier_id, :category)
     end
 end
--- studio_ghibli_store/db/schema.rb
+++ studio_ghibli_store/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_000528) do
+ActiveRecord::Schema.define(version: 2019_12_05_215553) do
 
   create_table "addresses", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_12_01_000528) do
     t.integer "supplier_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["categories_id"], name: "index_products_on_categories_id"
     t.index ["supplier_id"], name: "index_products_on_supplier_id"
   end
